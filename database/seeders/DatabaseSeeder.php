d1ddc0298484c1b28215fdf854a165dffc7dc84b
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2023-03-18 11:45:03 -0400

    seeder e factories para o db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             //UserSeeder::class,
-            //PlanSeeder::class,
+            PlanSeeder::class,
             SignatureSeeder::class
         ]);
     }
